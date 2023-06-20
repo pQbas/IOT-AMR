00eb86259639ed137b9b1e1a05b633232ec13622
pQbas <percybrayan18@gmail.com>
2023-06-20 12:13:34 -0500

    Add jquery to index.html

--- src/app/app.py
+++ src/app/app.py
@@ -1,13 +1,20 @@
 import sys
 sys.path.append('/home/pqbas/miniconda3/envs/iot/lib/python3.8/site-packages')
 from watchdog.events import EVENT_TYPE_OPENED
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/datos',methods=['GET'])
+def solicitud():
+    return jsonify({
+        'frutas': ['pera', 'manzana', 'mandarina', 'mango']
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
