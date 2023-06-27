a0023493c20f96d3f865e811a6b5ba542742b8eb
pQbas <percybrayan18@gmail.com>
2023-06-27 11:30:42 -0500

    Path /camera was added to show video

--- src/app/app.py
+++ src/app/app.py
@@ -2,10 +2,18 @@
 import sys
 sys.path.append('/home/pqbas/miniconda3/envs/iot/lib/python3.8/site-packages') #JORGE
 from watchdog.events import EVENT_TYPE_OPENED
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, Response
 import random
 import threading
+import time
+import logging
+import signal
+import os
+import cv2
 
+
+from background_thread import BackgroundThreadFactory, TASKS_QUEUE
+logging.basicConfig(level=logging.INFO, force=True)
 
 
 SIMULATOR = True # True / False - Verdadero si quiere usar el simulador
@@ -43,6 +51,8 @@ DICTIONARY = [{
 	}
 ]
 
+
+
 def subscriber_ros():
     
     if SIMULATOR == True:
@@ -72,21 +82,76 @@ def subscriber_ros():
 
 
 
-app = Flask(__name__)
+def obtener_imagen_camara():
+    # Crea un objeto VideoCapture para acceder a la cámara
+    video = cv2.VideoCapture(0)
+    
+    while True:
+        # Captura el fotograma actual de la cámara
+        ret, frame = video.read()
+        
+        # Convierte el fotograma a formato JPEG
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        
+        # Genera el fotograma codificado en bytes
+        frame_bytes = jpeg.tobytes()
+        
+        # Devuelve el fotograma como respuesta al cliente
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/functions.js')
-def functions():
-    return render_template('functions.js')
+def create_app():
 
-@app.route('/datos',methods=['GET'])
-def solicitud():
-    return jsonify(DICTIONARY)
+    app = Flask(__name__)
 
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/functions.js')
+    def functions():
+        return render_template('functions.js')
+
+    @app.route('/datos',methods=['GET'])
+    def solicitud():
+        return jsonify(DICTIONARY)
+    
+    @app.route('/camera')
+    def imagen_camara():
+        return Response(obtener_imagen_camara(),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    
+    notification_thread = BackgroundThreadFactory.create('notification')
+
+    # this condition is needed to prevent creating duplicated thread in Flask debug mode
+    if not (app.debug == 'development') == 'true':
+        notification_thread.start()
+
+        original_handler = signal.getsignal(signal.SIGINT)
+
+        def sigint_handler(signum, frame):
+            notification_thread.stop()
+
+            # wait until thread is finished
+            if notification_thread.is_alive():
+                notification_thread.join()
+
+            original_handler(signum, frame)
+
+        try:
+            signal.signal(signal.SIGINT, sigint_handler)
+        except ValueError as e:
+            logging.error(f'{e}. Continuing execution...')
+
+    return app
 
 if __name__ == '__main__':
     #subscriber_ros()
+    # Start the threa
+    #app.run(debug=True, port=5000)
+    
+    app = create_app()
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     app.run(debug=True, port=5000)
